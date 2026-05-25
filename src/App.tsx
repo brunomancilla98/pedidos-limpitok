3a04291b77af5ab81e3033ca6cce278d02492217
brunomancilla98 <bruno.mancilla98@gmail.com>
2026-05-25 15:28:05 -0300

    corregir import react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import logoLimpitok from "./assets/logo-limpitok.jpeg";
 
 const WHATSAPP = "543364029927";
